09112484756c693faea2fae04db9bcb860bbcea7
Noah Manneschmidt <noah@manneschmidt.net>
2024-04-01 14:53:40 -0700

    enable auto-secure local builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   base: "",
   server: {
     port: 5000,
