4cbd7b739cb3438f7d2e219ebf638e62d3757376
Noah Manneschmidt <noah@manneschmidt.net>
2025-01-12 15:18:59 -0800

    remove logrocket

--- ui/index.tsx
+++ ui/index.tsx
@@ -1,12 +1,9 @@
 import * as Bacon from "baconjs";
 import { Provider } from "jotai";
-import LogRocket from "logrocket";
 import { createRoot } from "react-dom/client";
 
 import { uiState } from "./state.ts";
 import { UI } from "./ui.tsx";
-
-LogRocket.init("rgozwj/smx-config-web");
 
 Bacon.fromEvent(document, "DOMContentLoaded").onValue(async () => {
   const rootEl = document.getElementById("root");
