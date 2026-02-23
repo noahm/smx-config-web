fc1aa663c28207536617dd964bb84fc04fb21cfa
Noah Manneschmidt <noah@manneschmidt.net>
2026-02-23 15:48:13 -0800

    restore use of our custom font

--- ui/index.tsx
+++ ui/index.tsx
@@ -1,10 +1,13 @@
 import * as Bacon from "baconjs";
 import { Provider } from "jotai";
 import { createRoot } from "react-dom/client";
-import { App } from "antd";
+import { App, ConfigProvider } from "antd";
 
 import { uiState } from "./state.ts";
 import { UI } from "./ui.tsx";
+
+const defaultFontFamilies =
+  "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'";
 
 Bacon.fromEvent(document, "DOMContentLoaded").onValue(async () => {
   const rootEl = document.getElementById("root");
@@ -12,9 +15,11 @@ Bacon.fromEvent(document, "DOMContentLoaded").onValue(async () => {
   const reactRoot = createRoot(rootEl);
   reactRoot.render(
     <Provider store={uiState}>
-      <App>
-        <UI />
-      </App>
+      <ConfigProvider theme={{ token: { fontFamily: `"Hanken Grotesk", ${defaultFontFamilies}` } }}>
+        <App>
+          <UI />
+        </App>
+      </ConfigProvider>
     </Provider>,
   );
 });
